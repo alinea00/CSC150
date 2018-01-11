1fbc23a33f57d721faa89901be72790a4f20b1a4
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-01-10 23:05:52 -0700

    Completed functionality and added comments.

--- Lab01/PetersenLab1.c
+++ Lab01/PetersenLab1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+/****************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program outputs a block of text with the
+ * course and assignment information with author's
+ * name and due date.
+ ****************************************************/
 int main()
 {
+    // output each line of text using printf function
     printf("#####################################\n");
     printf("#      Programming Assignment 1     #\n");
     printf("#     CSC 150 Computer Science I    #\n");
-    printf("#           Author: ???             #\n");
+    printf("#      Author: Heather Petersen     #\n");
     printf("#     Due Date: January 17, 2018    #\n");
     printf("#####################################\n");
-    
+
+    // return success code
     return 0;
 }
