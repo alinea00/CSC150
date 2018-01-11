8dbc4dc9d37abfd83ce2e4cf0493ee2aaa210b2d
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-01-10 22:54:41 -0700

    add initial output block

--- Lab01/PetersenLab1.c
+++ Lab01/PetersenLab1.c
@@ -2,7 +2,12 @@
 
 int main()
 {
-    printf("I am alive! Beware.\n");
-    getchar();
+    printf("#####################################\n");
+    printf("#      Programming Assignment 1     #\n");
+    printf("#     CSC 150 Computer Science I    #\n");
+    printf("#           Author: ???             #\n");
+    printf("#     Due Date: January 17, 2018    #\n");
+    printf("#####################################\n");
+    
     return 0;
 }
