a5174565c9c7eeb1037512f38ec2926c2cc1150e
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-01-19 20:52:54 -0700

    Starting lab 2

---
+++ Lab02/PetersenLab2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/****************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program asks the user to enter two numbers, 
+ * obtains them from the user and prints their sum, 
+ * difference, product, quotient and remainder in 
+ * that order.
+ ****************************************************/
+int main()
+{
+    
+
+    // return success code
+    return 0;
+}
