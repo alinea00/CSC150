80f101805deba084b380bf8741502909bb5da692
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-02-08 22:21:12 -0700

    Setup Lab 5

---
+++ Lab05/Lab05-Largest/PetersenLab5-Largest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/***************************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program will input a series of 10 non-negative numbers 
+ * then determine and print the largest of the numbers.
+ ***************************************************************/
+int main()
+{
+    
+    // return success code
+    return 0;
+}
---
+++ Lab05/Lab05-Multiples/PetersenLab5-Multiples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/***************************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program will use a loop to print the first 5 multiples 
+ * of the numbers 1 through 10. The multiples for each number 
+ * will be separated by tab characters.
+ ***************************************************************/
+int main()
+{
+    
+    // return success code
+    return 0;
+}
