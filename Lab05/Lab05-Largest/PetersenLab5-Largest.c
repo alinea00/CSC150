d8376a99d89f3a90418458922e29fd8e2364d771
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-02-10 20:10:34 -0700

    implement largest program

--- Lab05/Lab05-Largest/PetersenLab5-Largest.c
+++ Lab05/Lab05-Largest/PetersenLab5-Largest.c
@@ -9,7 +9,32 @@
  ***************************************************************/
 int main()
 {
-    
+    // Declare and initialize all variables
+    unsigned int counter = 1; 
+    unsigned int number = 0;
+    unsigned int largest = 0;
+
+    // Run the loop 10 times
+    while (counter <= 10)
+    {
+        // Prompt user for number
+        printf("Enter number %d: ", counter);
+        scanf("%d", &number);
+        
+        // Check if number is greater than largest and if so
+        // make it the new largest
+        if (number > largest)
+        {
+            largest = number;
+        }
+        
+        // Increment counter for next loop
+        counter++;
+    }
+  
+    // Output largest number 
+    printf("\nThe larget number is: %d\n", largest);
+
     // return success code
     return 0;
 }
