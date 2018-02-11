86c11ccd5dad3cd488dfeee760e424864befbe32
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-02-10 22:03:55 -0700

    Implement multiples program

--- Lab05/Lab05-Multiples/PetersenLab5-Multiples.c
+++ Lab05/Lab05-Multiples/PetersenLab5-Multiples.c
@@ -10,7 +10,41 @@
  ***************************************************************/
 int main()
 {
-    
+    // Declare and initialize all variables
+    int row = 1; 
+    int column = 1;
+    int multiple = 0;
+
+    // Print header columns
+    printf("Number\t1st\t2nd\t3rd\t4th\t5th\n");
+
+    // Loop for 10 rows
+    while (row <= 10)
+    {
+        // Print row number 
+        printf("%d\t", row);
+
+        // Loop for 5 columns
+        while (column <= 5)
+        {
+            // Calculate multiple
+            multiple = row * column;
+
+            // Print multiple in appropriate column
+            printf("%d\t", multiple);
+
+            // Increment column index
+            column++;
+        }
+
+        // Reset column index to 1 and increment row index
+        column = 1;
+        row++;
+
+        // Ensure next row starts on new line
+        printf("\n");
+    }
+
     // return success code
     return 0;
 }
