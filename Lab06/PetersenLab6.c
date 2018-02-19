74fc33d2c530fff9461db8583064e63e706f2061
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-02-18 20:37:18 -0700

    Set up lab 6

---
+++ Lab06/PetersenLab6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/***************************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program will ask a user how many grades they wish to 
+ * enter.  It will use a loop to get that many grade items from 
+ * the user. Once all grades are entered, it calculates the 
+ * average grade and uses an if...else if statement to print 
+ * the corresponding letter grade.
+ ***************************************************************/
+int main()
+{
+    
+
+    // return success code
+    return 0;
+}
