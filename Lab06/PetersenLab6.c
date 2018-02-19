92f2379dc13a6007561edc6030498aade870de5d
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-02-18 21:00:57 -0700

    Add core functionality to calculate percentage

--- Lab06/PetersenLab6.c
+++ Lab06/PetersenLab6.c
@@ -12,7 +12,32 @@
  ***************************************************************/
 int main()
 {
+    // Declare and initialize variables
+    unsigned int numGrades = 0;
+    float totalPoints = 0.0;
+    float gradeItem = 0.0;
+    float finalGrade = 0.0;
+
+    // Prompt user for number of grades to enter
+    printf("How many grade items would you like to enter? ");
+    scanf("%d", &numGrades);
+
+    // Loop for number of grades
+    for(int i = 1; i <= numGrades; i++)
+    {
+        // Prompt user for grade item
+        printf("Enter the grade for grade item number %d: ", i);
+        scanf("%f", &gradeItem);
+
+        // Add grade to accumulated total of points
+        totalPoints += gradeItem;
+    }
+
+    // Calculate final grade average
+    finalGrade = totalPoints / numGrades;
     
+    // Output final grade precentage
+    printf("\nAverage grade: %.2f%%\n", finalGrade);
 
     // return success code
     return 0;
