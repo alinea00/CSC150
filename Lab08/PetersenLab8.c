6c7022223553d3a2346258fb43ffb1774344c911
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-03-17 23:49:49 -0600

    Add conditional character string output

--- Lab08/PetersenLab8.c
+++ Lab08/PetersenLab8.c
@@ -19,6 +19,7 @@ int main()
     int extraExercise = 0.0;
     float ratePerDay = 0.0;
     float monthlyCharges = 0.0;   
+    const char* extraExerciseString = "";
 
     // Prompt user for how much their dog weighs in pounds
     printf("How many pounds does your dog weigh?:\t");
@@ -75,13 +76,18 @@ int main()
         {
             ratePerDay += 2.00;
         }
+
+        // This adds the extra exercise string for the output
+        // This is required in order to do the output in one
+        // statement instead of two using an if-else.
+        extraExerciseString = " with extra exercise"; 
     }
 
     // Calculate monthly charges
     monthlyCharges = ratePerDay * daysPerMonth;
 
     // Output calculation description, rate per day, and monthly charges
-    printf("\nTo board a %d pound dog for %d days:\n", weightInPounds, daysPerMonth);
+    printf("\nTo board a %d pound dog for %d days%s:\n", weightInPounds, daysPerMonth, extraExerciseString);
     printf("Rate per day:\t\t$%.2f\n", ratePerDay);
     printf("Monthly charges:\t$%.2f\n", monthlyCharges);
 
