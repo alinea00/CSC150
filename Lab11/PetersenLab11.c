32f122777c51358d2321939ed98e03a7e64b09ac
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-04-15 23:05:24 -0600

    Implemented functionality for Lab 11.

--- Lab11/PetersenLab11.c
+++ Lab11/PetersenLab11.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-// Function prototypes
-
+#define SIZE 11
 
 /***************************************************************
  * Author: Heather Petersen
  * Class:  CSC 150 Computer Science I
  * 
- * This program will 
+ * This program will prompt the user to enter a string, and 
+ * then print each character of the string on a new line. Then 
+ * print each character in reverse order, with the letters
+ * separated by hyphens.
  ***************************************************************/
 int main()
 {  
     // Declare and initialize variables    
- 
+    char inputString[SIZE];
+    int stringLength = 0;
+
+    printf("Enter a string: ");
+    scanf("%10s", inputString);
+    stringLength = strlen(inputString);
+
+    printf("The string with each character on a new line:\n");
+    for(int i = 0; i < stringLength; i++)
+    {
+        printf("%c\n", inputString[i]);
+    }
+    printf("\n");
+
+    printf("The string printed backwards and separated by hyphens:\n");
+    for(int i = stringLength - 1; i >= 0; i--)
+    {
+        printf("%c", inputString[i]);
+        printf("%c", i > 0 ? '-' : ' ');
+    }
+    printf("\n");
+
     // return success code
     return 0;
 }
