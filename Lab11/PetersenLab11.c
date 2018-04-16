9add510cf19fbd463ef1d110fc799a61ef737244
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-04-15 23:12:43 -0600

    Add comments to lab 11

--- Lab11/PetersenLab11.c
+++ Lab11/PetersenLab11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// Define symbolic constant for max string size
 #define SIZE 11
 
 /***************************************************************
@@ -18,10 +19,15 @@ int main()
     char inputString[SIZE];
     int stringLength = 0;
 
+    // Prompt user for string
     printf("Enter a string: ");
+    // Ensure only up to 10 characters are allowed
     scanf("%10s", inputString);
+
+    // Get length of strong
     stringLength = strlen(inputString);
 
+    // Write each character of the string out on its own line
     printf("The string with each character on a new line:\n");
     for(int i = 0; i < stringLength; i++)
     {
@@ -29,10 +35,12 @@ int main()
     }
     printf("\n");
 
+    // Write the string backwards with hyphens between letters
     printf("The string printed backwards and separated by hyphens:\n");
     for(int i = stringLength - 1; i >= 0; i--)
     {
         printf("%c", inputString[i]);
+        // Write hyphen except on last letter
         printf("%c", i > 0 ? '-' : ' ');
     }
     printf("\n");
