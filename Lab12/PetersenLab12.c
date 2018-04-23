1aeecb380320275a97781254b41e21ec121f3d8c
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-04-22 22:26:52 -0600

    Add grade entry and rough display

--- Lab12/PetersenLab12.c
+++ Lab12/PetersenLab12.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NUM_GRADES 8
+
+// Function prototypes
+void sortAscending(float data[], int dataSize);
+
 /***************************************************************
  * Author: Heather Petersen
  * Class:  CSC 150 Computer Science I
@@ -15,9 +20,25 @@
 int main()
 {  
     // Declare and initialize variables    
+    int grades[NUM_GRADES] = {0};
 
-   
+    for (int i = 0; i < NUM_GRADES; i++)
+    {
+        printf("Enter grade %d: ", i + 1);
+        scanf("%d", &grades[i]);
+    }
+
+
+    for (int i = 0; i < NUM_GRADES; i++)
+    {
+        printf("%d - X\n", grades[i]);
+    }
 
     // return success code
     return 0;
+}
+
+void sortAscending(float data[], int dataSize)
+{
+
 }
