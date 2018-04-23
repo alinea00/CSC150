922743a2e5171453d1b47b8c46a4136db6c20308
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-04-22 22:04:31 -0600

    Created lab 12 outline

---
+++ Lab12/PetersenLab12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/***************************************************************
+ * Author: Heather Petersen
+ * Class:  CSC 150 Computer Science I
+ * 
+ * This program will prompt the user for eight numeric grades.
+ * rint the eight scores in ascending order along with the 
+ * corresponding letter grade for each score.  After all numeric 
+ * and letter grades are displayed, the average numeric grade 
+ * will be shown to one decimal place with a percent sign,
+ * and the letter grade that corresponds to the average.
+ ***************************************************************/
+int main()
+{  
+    // Declare and initialize variables    
+
+   
+
+    // return success code
+    return 0;
+}
