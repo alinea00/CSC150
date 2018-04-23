f1021dc2102c4e79a3fb3b217d5d2c98a65bd2e5
Heather Petersen <heather.petersen@trojans.dsu.edu>
2018-04-22 23:02:39 -0600

    Implemented letter grade calculation

--- Lab12/PetersenLab12.c
+++ Lab12/PetersenLab12.c
@@ -5,6 +5,7 @@
 
 // Function prototypes
 void sortAscending(unsigned int data[], unsigned int dataSize);
+char calculateLetterGrade(float numericGrade);
 
 /***************************************************************
  * Author: Heather Petersen
@@ -21,6 +22,7 @@ int main()
 {  
     // Declare and initialize variables    
     unsigned int grades[NUM_GRADES] = {0};
+    char letterGrade = 'F';
 
     for (int i = 0; i < NUM_GRADES; i++)
     {
@@ -32,7 +34,8 @@ int main()
 
     for (int i = 0; i < NUM_GRADES; i++)
     {
-        printf("%d - X\n", grades[i]);
+        letterGrade = calculateLetterGrade(grades[i]);
+        printf("%d - %c\n", grades[i], letterGrade);
     }
 
     // return success code
@@ -53,4 +56,33 @@ void sortAscending(unsigned int data[], unsigned int dataSize)
             }
         }
     }
+}
+
+char calculateLetterGrade(float numericGrade)
+{
+    char letterGrade = 'F';
+
+    // Calculate letter grade.  
+    if(numericGrade >= 90.0)
+    {
+        letterGrade = 'A';
+    }
+    else if(numericGrade >= 80.0)
+    {
+        letterGrade = 'B';
+    }
+    else if(numericGrade >= 70.0)
+    {
+        letterGrade = 'C';
+    }
+    else if(numericGrade >= 60.0)
+    {
+        letterGrade = 'D';
+    }
+    else 
+    {
+        letterGrade = 'F';
+    }
+
+    return letterGrade;
 }
